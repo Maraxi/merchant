109837c083879b33bbe166e2ad77c37509b3e01b
Maraxi <Maraxi@users.noreply.github.com>
2021-11-18 06:09:00 +0100

    Add more tests.

--- tests/test_merchants_guide.py
+++ tests/test_merchants_guide.py
@@ -1,9 +1,16 @@
 """Tester for the merchants guide to the galaxy."""
 import subprocess
+from contextlib import contextmanager
 
 import pytest
 
 from merchant import merchants_guide
+
+
+@contextmanager
+def does_not_raise():
+    """No exception may be raised."""
+    yield
 
 
 @pytest.fixture
@@ -13,9 +20,8 @@ def guide():
 
 
 @pytest.fixture
-def guide_with_numbers():
+def guide_with_numbers(guide):
     """Supply an instance of Guide with some defined numbers."""
-    guide = merchants_guide.Guide()
     values = (
         ("i", "I"),
         ("v", "V"),
@@ -30,6 +36,19 @@ def guide_with_numbers():
     return guide
 
 
+@pytest.fixture
+def guide_with_prices(guide_with_numbers):
+    """Supply an instance of Guide with some defined numbers and prices."""
+    values = (
+        (("Silver", "Rocks"), 36.0),
+        (("Gold", "Rocks"), 136.0),
+        (("Silver", "Coins"), 5.0),
+    )
+    for pair, value in values:
+        guide_with_numbers._parse_price_definition(["i", pair[0], "is", value, pair[1]])
+    return guide_with_numbers
+
+
 @pytest.mark.parametrize(
     "test_input, expected_output",
     [
@@ -40,10 +59,109 @@ def guide_with_numbers():
     ],
 )
 def test_list_to_number(guide_with_numbers, test_input, expected_output):
-    """Test the consume function."""
+    """Test the list to number conversion."""
     assert guide_with_numbers._list_to_number(test_input) == expected_output
 
 
+@pytest.mark.parametrize(
+    "test_input, bool, expected_output",
+    [
+        ([], True, 1),
+        ([], False, 0),
+    ],
+)
+def test_list_to_number_empty_list(guide, test_input, bool, expected_output):
+    """Test the list to number conversion for empty lists."""
+    assert guide._list_to_number(test_input, bool) == expected_output
+
+
+@pytest.mark.parametrize(
+    "test_input, expectation",
+    [
+        (["foo", "is", "L"], does_not_raise()),
+        (["foobar", "bii", "baz"], pytest.raises(AssertionError)),
+        (["foo", "is", "Q"], pytest.raises(AssertionError)),
+    ],
+)
+def test_parse_number_definition(guide, test_input, expectation):
+    """Test the number parsing."""
+    with expectation:
+        guide._parse_number_definition(test_input)
+        assert len(guide.numbers) > 0
+
+
+@pytest.mark.parametrize(
+    "test_input, expectation",
+    [
+        (["foo"], pytest.raises(AssertionError)),
+        (["glog", "silver", "17", "Coins"], pytest.raises(AssertionError)),
+        (["glog", "silver", "is", "17", "Coins"], pytest.raises(KeyError)),
+        (["silver", "is", "17", "Coins"], does_not_raise()),
+        (["silver", "is", "abc", "Coins"], pytest.raises(ValueError)),
+    ],
+)
+def test_parse_price_definition(guide, test_input, expectation):
+    """Test the price parsing."""
+    with expectation:
+        guide._parse_price_definition(test_input)
+        assert len(guide.prices) > 0
+
+
+@pytest.mark.parametrize(
+    "test_input, expected_output, expectation",
+    [
+        (["how", "much", "is", "?"], "is 0", does_not_raise()),
+        (["how", "much", "is", "foo", "?"], "is 50", does_not_raise()),
+        (
+            ["how", "much", "is", "Glob", "Glob", "gLOB", "i", "?"],
+            "is 3001",
+            pytest.raises(KeyError),
+        ),
+        (
+            ["how", "much", "is", "Glob", "Glob", "Glob", "i", "?"],
+            "is 3001",
+            does_not_raise(),
+        ),
+        (["how", "much", "is", "i", "Glob", "?"], "is 3001", pytest.raises(ValueError)),
+    ],
+)
+def test_parse_number_query(
+    guide_with_numbers, test_input, expected_output, expectation
+):
+    """Test the number querying."""
+    with expectation:
+        assert guide_with_numbers._parse_number_query(test_input).endswith(
+            expected_output
+        )
+
+
+@pytest.mark.parametrize(
+    "test_input, expected_output, expectation",
+    [
+        (
+            ["how", "many", "Rocks", "is", "Glob", "Gold", "?"],
+            "Glob Gold is 136000.0 Rocks",
+            does_not_raise(),
+        ),
+        (
+            ["how", "many", "Coins", "is", "Glob", "Gold", "?"],
+            "",
+            pytest.raises(KeyError),
+        ),
+        (
+            ["how", "much", "Rocks", "is", "Glob", "Gold", "?"],
+            "",
+            pytest.raises(AssertionError),
+        ),
+    ],
+)
+def test_parse_price_query(guide_with_prices, test_input, expected_output, expectation):
+    """Test the price querying."""
+    with expectation:
+        assert guide_with_prices._parse_price_query(test_input) == expected_output
+
+
+@pytest.mark.xfail()
 def test_input():
     """Run a full test."""
     p = subprocess.run(
