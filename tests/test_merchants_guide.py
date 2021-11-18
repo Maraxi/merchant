47365fb273f3addd8189d0525ae92865087cf4b3
Maraxi <Maraxi@users.noreply.github.com>
2021-11-18 03:41:35 +0100

    Add some Tests.

--- tests/test_merchants_guide.py
+++ tests/test_merchants_guide.py
@@ -8,16 +8,42 @@ from merchant import merchants_guide
 
 @pytest.fixture
 def guide():
-    """Supply an empty instance of the guide class."""
+    """Supply an empty instance of Guide."""
     return merchants_guide.Guide()
 
 
-def test_consume(guide):
+@pytest.fixture
+def guide_with_numbers():
+    """Supply an instance of Guide with some defined numbers."""
+    guide = merchants_guide.Guide()
+    values = (
+        ("i", "I"),
+        ("v", "V"),
+        ("x", "X"),
+        ("foo", "L"),
+        ("bar", "L"),
+        ("foobar", "L"),
+        ("Glob", "M"),
+    )
+    for name, value in values:
+        guide._parse_number_definition([name, "is", value])
+    return guide
+
+
+@pytest.mark.parametrize(
+    "test_input, expected_output",
+    [
+        (["i"], 1),
+        (["foobar"], 50),
+        (["foo"], 50),
+        (["Glob", "foo", "i", "x"], 1059),
+    ],
+)
+def test_list_to_number(guide_with_numbers, test_input, expected_output):
     """Test the consume function."""
-    assert guide.consume("") == ""
+    assert guide_with_numbers._list_to_number(test_input) == expected_output
 
 
-@pytest.mark.xfail(reason="not implemented yet")
 def test_input():
     """Run a full test."""
     p = subprocess.run(
@@ -25,4 +51,10 @@ def test_input():
         capture_output=True,
         text=True,
     )
-    assert "Gold is 57800 Credits" in p.stdout
+    assert "pish tegj glob glob is 42" in p.stdout
+    assert "glob prok Gold is 57800.0 Credits" in p.stdout
+    assert (
+        "> how much wood could a woodchuck chuck if a woodchuck could chuck wood ?\n\
+I have no idea what you are talking about"
+        in p.stdout
+    )
